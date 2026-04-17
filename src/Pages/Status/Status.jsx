b0b4b5d34728eb80ff6c16c5a68c79e253440c1a
mohammadmostofa <mohammadmostofa.dev@gmail.com>
2026-04-17 19:40:29 +0600

    added in fill stock of color with name

--- src/Pages/Status/Status.jsx
+++ src/Pages/Status/Status.jsx
@@ -12,12 +12,15 @@ const Status = () => {
   const videoCount = mark.filter(item => item.type === 'video').length;
 
   const chartData = [
-    {name:"call",  value:callCount  || 0.01 },
-    {name:"text" , value:textCount  || 0.01 },
-    {name:"video", value:videoCount || 0.01 }
+    {name:"call",  value:callCount  || 0.01 , fill:"#05783B"   },
+    {name:"text" , value:textCount  || 0.01 , fill:" #780552"  },
+    {name:"video", value:videoCount || 0.01 , fill:" #1A0578 " }
   ];
   
   return (
+
+    <div>
+
     <div className= { `flex justify-center items-center py-5}`} >
 
       <PieChart style={{ width: '80%', maxWidth: '500px', maxHeight: '80vh', aspectRatio: 1 }} responsive>
@@ -27,7 +30,7 @@ const Status = () => {
         outerRadius="100%"
         // Corner radius is the rounded edge of each pie slice
         cornerRadius="50%"
-        fill="#8884d8"
+        fill=""
         // padding angle is the gap between each pie slice
         paddingAngle={5}
         dataKey="value"
@@ -38,6 +41,17 @@ const Status = () => {
     </PieChart>
 
     </div>
+
+    <div className='flex justify-center items-center gap-4 py-10'>
+      
+       <h2 className='flex gap-1'>  <span className='text-md font-bold '>Call</span> <span className='border  border-gray-800 rounded-sm px-3  outline-none bg-[#05783B]' ></span> </h2>
+       <h2 className='flex gap-1'>  <span className='text-md font-bold '>Text</span> <span className='border  border-gray-800 rounded-sm px-3  outline-none bg-[#780552]' ></span> </h2>
+       <h2 className='flex gap-1'>  <span className='text-md font-bold '>video</span> <span className='border border-gray-800 rounded-sm px-3 outline-none bg-[#1A0578] '></span> </h2>
+    
+    </div>
+
+    </div>
+
   );
 };
 
