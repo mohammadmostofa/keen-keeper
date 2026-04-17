408edafd5a67a6e0245367f72a6e5da2fe9cd716
mohammadmostofa <mohammadmostofa.dev@gmail.com>
2026-04-17 17:09:14 +0600

    added in fillter sorting drop menu

--- src/Component/Item/Item.jsx
+++ src/Component/Item/Item.jsx
@@ -4,11 +4,13 @@ import { MdOutlineTextsms } from 'react-icons/md';
 
 const Item = ({card}) => {
 
-  
+
   
   return (
-    <div className='mx-auto container'>
+    
+    <div>
 
+        
           <div className='border shadow-2xl p-4 w-full my-4 rounded-2xl  bg-gray-800'>
                 <h3> <span className='text-green-500 font-bold'> {card.type} </span> with {card.name} </h3>
                 <h2> {card.time} </h2>
--- src/Pages/Timeline/Timeline.jsx
+++ src/Pages/Timeline/Timeline.jsx
@@ -1,20 +1,50 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { ContextCard } from '../../Context/ContextProvider';
 import { IoCall } from 'react-icons/io5';
 import Item from '../../Component/Item/Item';
+import { NavLink } from 'react-router';
 
 
 const Timeline = () => {
-
   const {mark} = useContext(ContextCard);
+
+  const  [sorting, setSorting] = useState('all') ;
+
+  const filterData = mark.filter((item) =>{
+      if(sorting ==='all' ) return  true;
+      return item.type === sorting;
+  } );
 
   
   return (
     
-    <div>
+    <div>     
+
+           <div className='container mx-auto'>
+
+                  <div className="dropdown dropdown-start container mx-auto ">
+  <div tabIndex={0} role="button" className="btn m-1">TimeLine</div>
+  <ul tabIndex="-1" className="dropdown-content menu bg-base-100 rounded-box z-1 w-52 p-2 shadow-sm">
+    
+    <NavLink>
+    <li onClick={()=> setSorting('all')}><a>All</a></li>
+    <li onClick={()=> setSorting('call')}><a>call</a></li>
+    <li onClick={() => setSorting('text')}><a>text</a></li>
+    <li onClick={() => setSorting('video')}><a>video</a></li>
+    </NavLink>
+
+  </ul>
+
+</div>
+             <div>
+                
             {
-              mark.map((card) => <Item key={card.id}  card={card} ></Item> )
+              filterData.map((card,index) => <Item key={index}  card={card} ></Item> )
             }
+             </div>
+
+           </div>
+
     </div>
   );
 };
